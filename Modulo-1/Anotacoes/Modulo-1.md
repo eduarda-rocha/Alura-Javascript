b62ddc68facf93d1d48eebfdc0bb99b0d47c87ed
Barbara <barbararocha@alunos.utfpr.edu.br>
2021-10-01 15:31:00 -0300

    Atualizações

--- Modulo-1/Anotacoes/Modulo-1.md
+++ Modulo-1/Anotacoes/Modulo-1.md
@@ -11,7 +11,23 @@
 ## Aula 2 - Tipos de variáveis 
 
 * `Const` e `let`
+
 * Case Sensitive
+
 * `parseInt` e `parseFloat`
+
 * Usando números com casa decimal
+
 * Atribuição de variáveis
+
+## Aula 3 - Trabalhando com arrays
+
+* Criação de Arrays `new Array()`
+
+* Inserção de itens em um array `push`
+
+* Remoção de itens em um array `splice` 
+
+* Palavras reservadas como por exemplo `new`
+
+* Indices do array (começa no zero), zero-based
---
+++ Modulo-1/Codigos/Aula-3-array.js
@@ -0,0 +1,19 @@
+// Criação do array:
+const destinos = new Array(
+    `Brasil`,
+    `Chile`,
+    `Japão`
+);
+console.log(destinos);
+
+// Adicionando um elemento no array:
+destinos.push(`Coreia`);
+console.log(destinos);
+
+// Removendo um elemento do array:
+destinos.splice(2,1); // o array começa no posicao 0, ou seja, é zero-based
+console.log(destinos);
+
+// Imprimir um elemento especifico do array:
+console.log(destinos[0]);
+console.log(destinos[2]);
