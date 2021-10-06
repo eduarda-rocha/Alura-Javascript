3f352c410e1e4f5cf622e04c2708c577e08d9e57
Barbara <barbararocha@alunos.utfpr.edu.br>
2021-10-06 13:32:50 -0300

    Atualizações

---
+++ Modulo-2/Anotacoes/Modulo-2.md
@@ -0,0 +1,12 @@
+# O que foi aprendido:
+
+## Aula 1 - Repeticao de código
+
+* Criando classes `class _nome_{}`
+
+* O que são atributos
+
+* Instância/Objeto
+
+* Operador `new`
+
---
+++ Modulo-2/Codigos/Aula-1-Classe.js
@@ -0,0 +1,19 @@
+//criação da classe
+
+class Cliente{
+    nome;
+    cpf;
+    agencia;
+    saldo;
+}
+
+//criação do objeto, instanciação de um classe  
+const cliente1 = new Cliente();
+
+//atribuição
+cliente1.nome = "Jubiscleison";
+cliente1.cpf = 12345161819;
+cliente1.agencia = 1001;
+cliente1.saldo = 10876.78
+
+console.log(cliente1);
