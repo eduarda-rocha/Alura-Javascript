64b2d3430355afece8ab860106ff1c7adc76d4f5
Barbara <barbararocha@alunos.utfpr.edu.br>
2021-10-08 15:47:34 -0300

    Atualização

--- Modulo-2/Codigos/Aula-3/index.js
+++ Modulo-2/Codigos/Aula-3/index.js
@@ -1,24 +1,22 @@
 import {Cliente} from "./Cliente.js";
 import {ContaCorrente} from "./ContaCorrente.js";
 
-const cliente1 = new Cliente();
+
 const ccVitor = new ContaCorrente();
-const cliente2 = new Cliente();
 const ccAline = new ContaCorrente();
 
-
-cliente1.nome = "Vitor";
-cliente1.cpf = 17622728793;
+ccVitor.cliente = new Cliente();
+ccVitor.cliente.nome = "Vitor";
+ccVitor.cliente.cpf = 17622728793;
 ccVitor.agencia = 1010;
+
 ccVitor.depositar(1000);
 
-cliente2.nome = "Alice";
-cliente2.cpf = 13298294893;
+ccAline.cliente = new Cliente();
+ccAline.cliente.nome = "Aline";
+ccAline.cliente.cpf = 1871817917
 ccAline.agencia = 2772;
 ccAline.depositar(1200);
-
-ccVitor.cliente = cliente1;
-ccAline.cliente = cliente2;
 
 ccVitor.transferir(100, ccAline);
 
